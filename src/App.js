96290415cccf5a8d9af90351b804f732a1f32ee6
Branden Sherman <bshermandeveloper@gmail.com>
2020-11-20 12:25:44 -0500

    styles(background): added halftone background

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 function App() {
   return (
     <div className='App'>
+      <div className='home-background'></div>
       <Header />
       <Home />
     </div>
